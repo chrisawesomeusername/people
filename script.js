5126504056b0a2337405d9d82bbe7f16fcbba9b4
chrisawesomeusername <chrisawesomeusername@gmail.com>
2020-02-10 21:05:51 +1300

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+var people = [
+  {
+    name: 'chris',
+    age: 27,
+    gender: 'm'
+  },
+  {
+    name: 'orgo',
+    age: 26,
+    gender: 'm'
+  }
+  {
+    name: 'holly',
+    age: 24,
+    gender: 'f'
+  },
+  {
+    name: 'robyn',
+    age: 28,
+    gender: 'f'
+  },
+  {
+    name: 'ryan',
+    age: 27,
+    gender: 'm'
+  },
+  {
+    name: 'frankie',
+    age: 25,
+    gender: 'f'
+  }
+];
